3bab11dfb3c596f91d3c3427add933bab723c862
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2023-06-21 18:31:19 +0100

    change home image alt

--- src/Home.js
+++ src/Home.js
@@ -22,7 +22,7 @@ const HomePage = () => {
              </div>
 
              <div className="home_img">
-              <img src="/image/Home_img.png" alt="Home Profile photo"/>
+              <img src="/image/Home_img.png" alt="Home Profile"/>
              </div>
              <div class="home_data">
                   <h1 className="home_title fade-in">Hi, <br/>I'm Praisejah</h1>
